92fe84de416b65bf01ad1b1b3f273f6670e90c0a
Jean Rodriguez <frankrodriguez9517@gmai.com>
2025-02-04 13:00:45 -0500

    input variables

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
+
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = var.project-id
+  region  = var.region
+  zone    = var.zone
 }
 
 ### NETWORK
 data "google_compute_network" "default" {
-  name                    = "default"
+  name = "default"
 }
 
 ## SUBNET
 resource "google_compute_subnetwork" "subnet-1" {
-  name                     = "subnet1"
-  ip_cidr_range            = "10.127.0.0/20"
+  name                     = var.subnet-name
+  ip_cidr_range            = var.subnet-cidr
   network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
-  private_ip_google_access = true
+  region                   = var.region
+  private_ip_google_access = var.private_google_access
 }
 
 resource "google_compute_firewall" "default" {
